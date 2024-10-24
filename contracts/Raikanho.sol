24499816626c7f17e92231199567190b0eedcfc3
Wazinga <ezechukwuemmanuel10@gmail.com>
2024-10-24 18:23:25 +0100

    First Commit

---
+++ contracts/Raikanho.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Raikanho is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("Raikanho", "Raika")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
